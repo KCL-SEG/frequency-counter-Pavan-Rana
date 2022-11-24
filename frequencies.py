858b57ea4f57dcddd920e35a651e26740f83b25f
Pavan-Rana <pavan.rana@kcl.ac.uk>
2022-11-24 16:48:43 +0000

    done

--- frequencies.py
+++ frequencies.py
@@ -5,7 +5,7 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     while(len(items)!=0):
-        frequencies.update({items[0],items:count(items[0])})
+        frequencies.update({items[0]:items.count(items[0])})
         temp = items.count(items[0])
         tem = items[0]
         for i in range(temp):
