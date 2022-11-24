cd69fc8d87ecc972bc7d8d9983353a7268dbd8d1
Pavan-Rana <pavan.rana@kcl.ac.uk>
2022-11-24 17:16:12 +0000

    done

--- frequencies.py
+++ frequencies.py
@@ -4,11 +4,15 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in range(len(items)):
+        if type(items[i]) is int:
+            items[i] = str(items[i])
+
     while(len(items)!=0):
-        frequencies.update({items[0]:items.count(items[0])})
+        frequencies.update({str(items[0]):items.count(items[0])})
         temp = items.count(items[0])
         tem = items[0]
-        for i in range(temp):
+        while(temp!=0):
             items.remove(tem)
-
+            temp = temp - 1
     return frequencies
