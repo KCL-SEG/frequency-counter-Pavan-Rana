171e3c1bcf9ac688e9cb6f3504f691fc664a07a1
Pavan-Rana <pavan.rana@kcl.ac.uk>
2022-11-24 16:36:47 +0000

    done

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in range(len(items)):
+        if frequencies.keys().count(items(i))==0:
+            frequencies.update({items(i):1})
+        else:
+            frequencies.update({items(i):frequencies.get(items(i))})
     return frequencies
